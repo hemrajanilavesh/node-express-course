c735d6f4384966391158f9d6dd4b897d31be077d
hemrajanilavesh <hemrajanilavesh@gmail.com>
2020-09-13 11:07:46 +0530

    Adding get users end point

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express();
 const port = 8080
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+]
+
+app.get('/users', function(request, response) {
+    response.json({
+        success: true,
+        message: 'Successfully got users',
+        users: mockUserData
+    })
+
+})
+
+
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 })
